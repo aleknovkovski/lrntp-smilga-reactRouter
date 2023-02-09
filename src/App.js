de79546f57287801d6edf2298d7cfd9188d38141
Alek Novkovski <alek@greatcode2.com>
2023-02-09 19:39:46 +0100

    Setup login/dashboard routes w user state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Products from './pages/Products';
 import Error from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
 import SingleProduct from "./pages/SingleProduct";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import {useState} from "react";
 
 function App() {
+    const [user, setUser] = useState(null);
     return (
         <BrowserRouter>
             <Routes>
@@ -15,6 +19,8 @@ function App() {
                     <Route path='about' element={<About />} />
                     <Route path='products' element={<Products />} />
                     <Route path='products/:productId' element={<SingleProduct />} />
+                    <Route path='login' element={<Login setUser={setUser} />} />
+                    <Route path='dashboard' element={<Dashboard user={user} />}/>
                     <Route path="*" element={<Error/>} />
                 </Route>
             </Routes>
