7e59aca1a1a7f7fb9a360a4f58e2fc1f2b854029
Alek Novkovski <alek@greatcode2.com>
2023-02-09 12:40:05 +0100

    Add static app-wide footer/navbar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Error from "./pages/Error";
 function App() {
     return (
         <BrowserRouter>
+            <nav>Our navbar</nav>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='about' element={<About />} />
                 <Route path='products' element={<Products />} />
                 <Route path="*" element={<Error/>} />
             </Routes>
+            <nav>Our footer</nav>
         </BrowserRouter>
     );
 }
