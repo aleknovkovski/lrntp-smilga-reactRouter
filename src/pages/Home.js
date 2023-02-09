430850c0bdf9f975dfe5c1d9c2118ae3f2a44cf8
Alek Novkovski <alek@greatcode2.com>
2023-02-09 15:59:45 +0100

    Implement Navbar, use on top of all Home subroutes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import {Link, Outlet} from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
     return (
         <section className='section'>
-            <h2>Home Page</h2>
+            <Navbar />
             <Outlet />
         </section>
     );
