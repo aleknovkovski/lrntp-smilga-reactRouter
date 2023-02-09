eb1cc74ae514ba5b8b9c60418eb2e71bd96fcf2b
Alek Novkovski <alek@greatcode2.com>
2023-02-09 13:55:55 +0100

    Remove static nav,foot; nest routes under root

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ import Error from "./pages/Error";
 function App() {
     return (
         <BrowserRouter>
-            <nav>Our navbar</nav>
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='about' element={<About />} />
-                <Route path='products' element={<Products />} />
-                <Route path="*" element={<Error/>} />
+                <Route path='/' element={<Home />}>
+                    <Route path='about' element={<About />} />
+                    <Route path='products' element={<Products />} />
+                    <Route path="*" element={<Error/>} />
+                </Route>
             </Routes>
-            <nav>Our footer</nav>
         </BrowserRouter>
     );
 }
