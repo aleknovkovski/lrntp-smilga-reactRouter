28d6865b46d6de54c9722b66773e24e1713384a2
Alek Novkovski <alek@greatcode2.com>
2023-02-09 16:20:26 +0100

    Add Index Route for homepage, move shared to Parent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Outlet} from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Products';
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />}>
+                <Route path='/' element={(<>
+                    <Navbar/>
+                    <Outlet/>
+                </>)}>
+                    <Route index element={<h2>Homepage content</h2>} />
                     <Route path='about' element={<About />} />
                     <Route path='products' element={<Products />} />
                     <Route path="*" element={<Error/>} />
