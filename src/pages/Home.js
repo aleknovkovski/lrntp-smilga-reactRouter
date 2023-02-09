4a5ddb6c176a11f0321d3fc36f97ef6487dc11d3
Alek Novkovski <alek@greatcode2.com>
2023-02-09 12:14:14 +0100

    Prettier reformat

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 const Home = () => {
-  return (
-    <section className='section'>
-      <h2>Home Page</h2>
-    </section>
-  );
+    return (
+        <section className='section'>
+            <h2>Home Page</h2>
+
+        </section>
+    );
 };
 export default Home;
